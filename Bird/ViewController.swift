d91970e2616d675f23818781efde30edff1c37a4
StIch0 <loluich@gmail.com>
2018-04-09 17:27:19 +0800

    SearchBirdBeginig

--- Bird/ViewController.swift
+++ Bird/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    var searchData = [String]()
     var viewData = [BirdViewData]()
     var activityIndicator : UIActivityIndicatorView = UIActivityIndicatorView()
     let presenter  = BirdPresenter(service: GeneralService())
@@ -17,6 +21,9 @@ class ViewController: UIViewController {
         activityIndicator.hidesWhenStopped = true
         presenter.atachView(view: self as ViewBuild)
         presenter.getData(APISelected.get_bird.rawValue, [:], withName: "", imageArr: [])
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -42,10 +49,24 @@ extension ViewController : ViewBuild {
 
     internal func setData(data: [ViewData]) {
         viewData = data as! [BirdViewData]
-        for (_, index) in viewData.enumerated(){
-            print("Index = ", index.birds)
-        }
+//        for (_, index) in viewData.enumerated(){
+//            print("Index = ", index.birds)
+//        }
+    }
+}
+
+extension ViewController : UITableViewDataSource, UITableViewDelegate {
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = searchData[indexPath.row]
+        return cell
     }
 
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewData.count
+    }
 
+    
 }
